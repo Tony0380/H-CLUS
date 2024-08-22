4bba1df6ace4f701e2d89883ae14930235836ba3
Antonio Colamartino <colamar95@outlook.it>
2024-08-22 17:19:15 +0200

    Refactor Cluster class to use Collection instead of array for storing real values

--- LAB3/src/clustering/Cluster.java
+++ LAB3/src/clustering/Cluster.java
@@ -1,68 +1,88 @@
 package src.clustering;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
 import src.data.Data;
 
-public class Cluster {	
+public class Cluster implements Iterable<Integer>, Cloneable{	
 
-	private Integer clusteredData[]=new Integer[0];
+	private Set<Integer> clusteredData=new TreeSet<>();
+
+    /**
+     * Implementazione del metodo virtuale iterator presente nell'interfaccia Iterable
+     * @return Iteratore per la Collection di tipo TreeSet clusteredData
+     */
+	public Iterator<Integer> iterator() {
+		return clusteredData.iterator();
+	}
 	
-	
-		
-	//add the index of a sample to the cluster
-	public void addData(int id){
-		// controllo duplicati
-		for(int i=0; i<clusteredData.length;i++)
-			if(id==clusteredData[i])
-				return;
-		Integer clusteredDataTemp[]=new Integer[clusteredData.length+1];
-		System.arraycopy(clusteredData, 0, clusteredDataTemp, 0, clusteredData.length);
-		clusteredData=clusteredDataTemp;
-		clusteredData[clusteredData.length-1]=id;			
+	/**
+	 * Aggiunge un indice al Cluster
+	 * @param id indice da aggiungere
+	 */
+	public void addData(int id) {
+		clusteredData.add(id);
 	}
 		
-	
+	/**
+	 * Restituisce la dimensione del cluster
+	 * @return dimensione del cluster
+	 */
 	public int getSize() {
-		return clusteredData.length;
+		return clusteredData.size();
 	}
 	
-	public int getElement(int i) {
-		return clusteredData[i];
+	/**
+	 * Implementazione del metodo clone della classe {@code Object}
+	 * @return Oggetto clonato
+	 * @throws CloneNotSupportedException Se l'oggetto non supporta l'interfaccia Clonable
+	 */
+	public Object clone() {
+		Object o = null;
+		try {
+			o = super.clone();
+		} catch (CloneNotSupportedException e) {
+			System.out.println("Errore: L'oggetto non può essere clonato, restituzione di un oggetto nullo");
+		}
+		return o;
 	}
 	
-	// crea una copia del cluster corrente
-	public Cluster createACopy() {
-			Cluster copyC=new Cluster();
-			for (int i=0;i<getSize();i++)
-				copyC.addData(clusteredData[i]);
-			return copyC;
-	}
-	
-	// crea un nuovo cluster che è la fusione dei due cluster pre-esistenti
+	/**
+	 * crea un nuovo cluster che è la fusione dei due cluster pre-esistenti
+	 * @param c Cluster da fondere col cluster {@code this}
+	 * @return Cluster risultante dalla fusione di {@code c & this}
+	 */
 	public Cluster mergeCluster (Cluster c)
 	{
-		Cluster newC=new Cluster();
-		for (int i=0;i<getSize();i++)
-			newC.addData(clusteredData[i]);
-		for (int i=0;i<c.getSize();i++)
-			newC.addData(c.clusteredData[i]);
-		return newC;
-		
+		Cluster newCluster = (Cluster) this.clone();
+		Iterator<Integer> i = c.iterator();
+		while(i.hasNext()) {
+			newCluster.clusteredData.add(i.next());
+		}
+		return newCluster;
 	}
 	
 	
 	public String toString() {		
 		String str="";
-		for (int i=0;i<clusteredData.length-1;i++)
-			str+=clusteredData[i]+",";
-		str+=clusteredData[clusteredData.length-1];
+		Iterator<Integer> i = this.iterator();
+
+		while(i.hasNext()) {
+			str+=i.next();
+			if(i.hasNext()) {
+				str+=",";
+			}
+		}
+
 		return str;	
 	}
 	
 	public String toString(Data data){
 		String str="";
 		
-		for(int i=0;i<clusteredData.length;i++)
-			str+="<"+data.getExample(clusteredData[i])+">";				
+		for(Integer id : clusteredData)
+			str+="<"+data.getExample(id)+">";				
 		
 		return str;
 		
