aa8e7d6c89a7a3eeb4f41a0d2ac5b70a332bf13f
Antonio Colamartino <colamar95@outlook.it>
2024-08-22 17:28:49 +0200

    Refactor SingleLinkDistance class to optimize distance calculation

--- LAB3/src/distance/SingleLinkDistance.java
+++ LAB3/src/distance/SingleLinkDistance.java
@@ -8,12 +8,13 @@ public class SingleLinkDistance implements ClusterDistance {
 	public double distance(Cluster c1, Cluster c2, Data d) {
 		
 		double min=Double.MAX_VALUE;
-		
+
 		Iterator<Integer> i1 = c1.iterator();
 		Iterator<Integer> i2 = c2.iterator();
 		while(i1.hasNext())
 		{
 			Example e1=d.getExample(i1.next());
+			
 			while(i2.hasNext()) {
 				double distance=e1.distance(d.getExample(i2.next()));
 				if (distance<min)				
