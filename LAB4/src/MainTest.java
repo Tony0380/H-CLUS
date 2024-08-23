d99135d4a2e37947bc35d0a79aee69dee49e9a23
Antonio Colamartino <colamar95@outlook.it>
2024-08-23 16:41:07 +0200

    Refactor HierachicalClusterMiner class to add serialization methods and implement Serializable interface in all composed classes

--- LAB4/src/MainTest.java
+++ LAB4/src/MainTest.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import src.clustering.HierachicalClusterMiner;
 import src.data.Data;
 import src.distance.*;
@@ -22,16 +24,68 @@ public class MainTest {
 				System.out.print(distancematrix[i][j]+"\t");
 			System.out.println("");
 		}
-		
-		System.out.print("Inserisci la profondita del dendrogramma da costruire: ");
-		int k = Keyboard.readInt();
-		HierachicalClusterMiner clustering=new HierachicalClusterMiner(k);
+
+		HierachicalClusterMiner clustering;
+
+		System.out.println("Scoprire un nuovo HierachicalClusterMiner o caricarlo da File?: ");
+		System.out.println("1. Scorprine uno nuovo");
+		System.out.println("2. Caricalo da file");
+		System.out.print("Scelta: ");
+		int choice = Keyboard.readInt();
+
+		if(choice == 1) {
+			System.out.print("Inserisci la profondita del dendrogramma da costruire: ");
+			int k = Keyboard.readInt();
+			clustering = new HierachicalClusterMiner(k);
+			System.out.print("Inserisci il nome / percorso del file su cui salvarlo: ");
+			String fileName = Keyboard.readString();
+
+                    try {
+                        clustering.salva(fileName);
+                    } catch (IOException ex) {
+						System.out.println("Errore nella scrittura su file, l'oggetto non è stato salvato.");
+                    }
+
+		} else if(choice == 2) {
+
+			System.out.print("Inserisci il nome / percorso del file da cui caricare il dendrogramma: ");
+			String fileName = Keyboard.readString();
+
+			try {
+
+				clustering = HierachicalClusterMiner.loaHierachicalClusterMiner(fileName);
+
+			} catch (FileNotFoundException e) {
+				
+				System.out.println("Errore nella lettura da file, file non trovato.");
+				System.out.println("Generazione di un dendrogramma di dimensione massima.");
+				clustering = new HierachicalClusterMiner(data.getNumberOfExample());
+
+			} catch (ClassNotFoundException e) {
+
+				System.out.println("Errore nella lettura da file, l'oggetto non è stato caricato. Sembra che su file sia salvato altro.");
+				System.out.println("Generazione di un dendrogramma di dimensione massima.");
+				clustering = new HierachicalClusterMiner(data.getNumberOfExample());
+
+			} catch (IOException e) {
+
+				System.out.println("Errore nella lettura da file, l'oggetto non è stato caricato.");
+				System.out.println("Generazione di un dendrogramma di dimensione massima.");
+				clustering = new HierachicalClusterMiner(data.getNumberOfExample());
+
+			}
+
+		} else {
+			System.out.println("Mi dispiace, le opzioni erano 1 e 2");
+			System.out.println("Generazione di un dendrogramma di dimensione massima.");
+			clustering = new HierachicalClusterMiner(data.getNumberOfExample());
+		}
 
 		System.out.println("Decidi che algoritmo di distanza utilizzare per calcolare il dendrogramma: ");
 		System.out.println("1. Single Link Distance");
 		System.out.println("2. Average Link Distance");
 		System.out.print("Scelta: ");
-		int choice = Keyboard.readInt();
+		choice = Keyboard.readInt();
 
 		if(choice == 1) {
 			System.out.println("Single link distance");
